ff5d1cd97b9a8b550a4bf5a6ea8ed80b584d0cd9
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-12-04 15:48:40 +0200

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -10,9 +10,74 @@ fn part1(s: &String) -> i32 {
     count
 }
 
+fn str_to_numeric(s: &str) -> char {
+    match s {
+        "one" => '1',
+        "two" => '2',
+        "three" => '3',
+        "four" => '4',
+        "five" => '5',
+        "six" => '6',
+        "seven" => '7',
+        "eight" => '8',
+        "nine" => '9',
+        _ => panic!("Should never happen"),
+    }
+}
+
+fn part2(s: &String) -> i32 {
+    let mut count = 0;
+    let numbers = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    for line in s.lines() {
+        let first = line
+            .char_indices()
+            .find(|(_, c)| c.is_numeric())
+            .unwrap_or((usize::MAX, '0'));
+
+        let second = line
+            .char_indices()
+            .rfind(|(_, c)| c.is_numeric())
+            .unwrap_or((usize::MIN, '0'));
+
+        let mut smallest = (usize::MAX, '0');
+        let mut largest = (usize::MIN, '0');
+
+        for &s in &numbers {
+            if let Some(i) = line.find(s) {
+                if i < smallest.0 {
+                    smallest = (i, str_to_numeric(&s))
+                }
+            };
+            if let Some(i) = line.rfind(s) {
+                if i + s.len() - 1 > largest.0 {
+                    largest = (i + s.len() - 1, str_to_numeric(&s))
+                }
+            }
+        }
+
+        let x = if first.0 < smallest.0 {
+            first.1
+        } else {
+            smallest.1
+        };
+        let y = if second.0 >= largest.0 {
+            second.1
+        } else {
+            largest.1
+        };
+
+        count += format!("{}{}", x, y).parse::<i32>().unwrap();
+    }
+
+    count
+}
 fn main() {
     let file_path = "./src/data";
     let s = fs::read_to_string(file_path).expect("File not found !!!");
 
     println!("Part 1 result: {}", part1(&s));
+    println!("Part 2 result: {}", part2(&s));
 }
